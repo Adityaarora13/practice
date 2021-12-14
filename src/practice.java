20639d6e1ae8b20c125340dd46a3b6b01df07e57
Aditya <aparora13@gmail.com>
2021-12-14 21:17:35 +0530

    1

--- src/practice.java
+++ src/practice.java
@@ -1,24 +1,32 @@
-class Mythread extends Thread
+class Mythread implements Runnable
 {
     public void run()
     {
-        System.out.println("i am thread 1");
+        while(true) {
+            System.out.println("good morning");
+        }
+
 
     }
 }
-class Mythread2 extends Thread
+class Mythread2 implements Runnable
 {
     public void run()
     {
-        System.out.println("i am thread 2");
+        while(true) {
+            System.out.println("welcome");
+        }
+
     }
 }
 public class practice {
     public static void main(String[] args) {
         Mythread t1 = new Mythread();
+        Thread a1 = new Thread(t1);
         Mythread2 t2 = new Mythread2();
-        t1.start();
-        t2.start();
+        Thread a2 = new Thread(t2);
+        a1.start();
+        a2.start();
 
     }
 }
